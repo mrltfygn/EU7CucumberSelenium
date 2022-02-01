119462b5e359d5c6d58722c54516ef97831cc421
mrltfygn <omeryegen88@gmail.com>
2022-02-01 20:48:25 +0100

    LoginStepDefs

--- src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -1,4 +1,25 @@
 package com.vytrack.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginStepDefs {
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters the driver information")
+    public void the_user_enters_the_driver_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should be able to login")
+    public void the_user_should_be_able_to_login() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
